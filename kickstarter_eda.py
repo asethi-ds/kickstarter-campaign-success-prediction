dfb4e70a5cb3ae57665c962f6090e227884323cc
asethi-ds <asethi@LAPTOP-SGHLL80B.localdomain>
2019-08-22 13:26:06 -0500

    eda code

---
+++ kickstarter_eda.py
@@ -0,0 +1,13 @@
+# Kickstarter
+# Data cleaning, preprocessing and exploratory analysis
+# Author Asethi
+# Last updated 8/22/2019
+
+# Importing packages
+import time
+start_time = time.time()
+import pandas as pd
+import numpy as np
+import datetime as dt
+import os
+import glob

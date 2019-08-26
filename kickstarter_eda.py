e315e3e783ba0b701eaf2d4ec4015ffe5b2fa852
asethi-ds <asethi@LAPTOP-SGHLL80B.localdomain>
2019-08-25 23:27:18 -0500

    data preprocessing functions added

--- kickstarter_eda.py
+++ kickstarter_eda.py
@@ -1,6 +1,6 @@
 # Kickstarter
 # Data cleaning, preprocessing and exploratory analysis
-# Author Asethi
+# Author asethi
 # Last updated 8/22/2019
 
 # Importing packages
@@ -35,6 +35,32 @@ def import_source_files(config_file_name):
     return all_files
 
 
+# Data Preprocessing
+def data_preprocess(kicksstarter_source_dataset):
+
+    kickstarter_source_dataset['state'].value_counts()
+
+    # Getting states suspended, cancelled, successful, failed
+    kickstarter_projects = kickstarter_source_dataset[(kickstarter_source_dataset['state'] == 'failed')|(kickstarter_source_dataset['state']
+    == 'successful')|(kickstarter_source_dataset['state'] == 'canceled')|(kickstarter_source_dataset['state'] == 'suspended')]
+
+    # Populating the states canceled, suspended, failed as successful
+    kickstarter_projects.loc[kickstarter_projects['state'] == 'canceled', 'state'] = 'unsuccessful'
+    kickstarter_projects.loc[kickstarter_projects['state'] == 'suspended', 'state'] = 'unsuccessful'
+    kickstarter_projects.loc[kickstarter_projects['state'] == 'failed', 'state'] = 'unsuccessful'
+
+    # We now have two states for the target variable, successful or unsuccessful
+    kickstarter_projects['state'].value_counts()
+
+    ((kickstarter_projects.isnull() | kickstarter_projects.isna()).sum() * 100 / kickstarter_projects.index.size).round(2)
+    # We see that 234 missing values in usd pledged, we populate these missing values using values from usd_pledged_real
+    kickstarter_projects['usd pledged'].fillna(kickstarter_projects.usd_pledged_real, inplace=True)
+    
+    return kickstarter_projects
+
+
+
+
 # Console for global variables and functions call
 config_file_name = 'loc_config.ini'
 
@@ -43,7 +69,9 @@ config_file_name = 'loc_config.ini'
 all_source_files=import_source_files(config_file_name)
 kickstarter_source_dataset=pd.read_csv(all_source_files[0], encoding='ISO-8859-1')
 
-print("--- %s seconds ---" % (time.time() - start_time))
 
-# Data Cleaning
+kickstarter_workset=data_preprocess(kickstarter_source_dataset)
+
+
+print("--- %s seconds ---" % (time.time() - start_time))
 
